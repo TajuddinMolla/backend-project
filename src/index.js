9087bbeb42ffb9e339222367d5d91e620ecf516b
Md. Tajuddin Molla <taj.unicart@gmail.com>
2026-05-11 21:20:27 +0600

    feat: initialize express server with dotenv configuration and nodemon dev script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
